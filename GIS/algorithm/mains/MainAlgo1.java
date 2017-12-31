3dd9413cbdb94dcf2e5b33e1b3b0847f000f2164
orelshalom <orelshalom1@gmail.com>
2017-12-31 11:37:39 +0200

    Test for the new branch htm

--- GIS/algorithm/mains/MainAlgo1.java
+++ GIS/algorithm/mains/MainAlgo1.java
@@ -22,7 +22,7 @@ import write.WriteFile;
  * "Note: If you use an IDE such as Eclipse or NetBeans, 
  * you will get a warning to ask you to close the input to prevent a potential resource leak
  * Ignore the warning because the input is automatically closed when your program is terminated. 
- * In this case, there will be no resource leaking." (page 39)
+ * In this case, there will be no resource leaking."wgd (page 39)
  * @author Orel and Samuel.
  */
 public class MainAlgo1 {
---
+++ GIS/util/try1.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class try1 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					try1 window = new try1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public try1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
